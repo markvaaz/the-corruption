4dd95994043465faa558d0ee9f13c4904fc59582
thevaaz <61412379+thevaaz@users.noreply.github.com>
2020-03-01 00:31:09 -0300

    Update tools.js

--- src/js/tools.js
+++ src/js/tools.js
@@ -1,27 +1,14 @@
 let config;
 let personagem;
-function create(elementName, attributes){
-  let element = document.createElement(elementName);
-  if(attributes !== undefined){
-    if (
-      attributes.className && element.setAttribute("class", attributes.className),
-      attributes.onclick && element.setAttribute("onclick", attributes.onclick),
-      attributes.href && element.setAttribute("href", attributes.href),
-      attributes.style && element.setAttribute("style", attributes.style),
-      attributes.name && element.setAttribute("name", attributes.name),
-      attributes.id && element.setAttribute("id", attributes.id),
-      attributes.innerHTML && (element.innerHTML = attributes.innerHTML),
-      element.value === "" && attributes.value && element.setAttribute("value", attributes.value),
-      element.placeholder === "" && attributes.placeholder && element.setAttribute("placeholder", attributes.placeholder),
-      attributes.custom && element.setAttribute(attributes.custom[0], attributes.custom[1])
-    );
-    if(typeof attributes.appendChild == "object"){
-      for(let i = 0; i < attributes.appendChild.length; i++){
-        element.appendChild(attributes.appendChild[i]);
-      }
+function create(el, atributos){
+  let elemento = document.createElement(el)
+  
+  for(let i in atributos){
+    if(elemento[i] !== null && elemento[i] !== undefined){
+      elemento[i] = atributos[i]
     }
   }
-  return element;
+  return elemento
 }
 Element.prototype.remove = function() {
   this.parentElement.removeChild(this);
